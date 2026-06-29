71ef17064dfdac06f527c3e63c8e4b8541408a19
HyperCube24 <hypercube24official@gmail.com>
2026-06-29 13:45:37 -0700

    Numbers 1-4 and tiles are semi-reliably checked for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import all required libraries
+from linecache import checkcache
 from random import randint
 from PIL import ImageGrab
 import pyscreeze
@@ -39,7 +40,7 @@ def moveClick(x,y,gridIn,shiftX=0,shiftY=0):
 
 # Returns the color from the provided grid position in the screenshot
 def grabColorGrid(x,y,gridIn,pixels,shiftX=0,shiftY=0):
-    xTest, yTest = toPosition(0, 0, gridIn, shiftX, shiftY)
+    xTest, yTest = toPosition(x, y, gridIn, shiftX, shiftY)
     return pixels[xTest,yTest]
 
 # Scans the board and determines what all of the squares are
@@ -47,7 +48,49 @@ def scanGrid(gridIn):
     img = ImageGrab.grab()
     img.save("testImage.png","PNG")
     pix = img.load()
-    print(grabColorGrid(0,0,gridIn,pix))
+    # Sets up the color variables (missing 5-8)
+    lightGreen = (170, 215, 81)
+    darkGreen = (162, 209, 73)
+    lightTan = (229, 194, 159)
+    darkTan = (215, 184, 153)
+    one = (25, 118, 210)
+    two = (56, 142, 60)
+    three = (211, 47, 47)
+    four = (123, 31, 162)
+    # Scans over the image and traces those locations with the mouse
+    checkXShift = 0.05
+    checkYShift = 0.24
+    for y in range(0,gridIn[5]):
+        printRow = ""
+        for x in range(0,gridIn[4]):
+            curColor = grabColorGrid(x, y, gridIn, pix, checkXShift, checkYShift)
+            if curColor == lightGreen:
+                printRow = printRow + "■"
+                delay = 0
+            elif curColor == darkGreen:
+                printRow = printRow + "□"
+                delay = 0
+            elif curColor == lightTan or curColor == darkTan:
+                printRow = printRow + "O"
+                delay = 0.25
+            elif curColor == one:
+                printRow = printRow + "1"
+                delay = 0.25
+            elif curColor == two:
+                printRow = printRow + "2"
+                delay = 0.25
+            elif curColor == three:
+                printRow = printRow + "3"
+                delay = 0.25
+            elif curColor == four:
+                printRow = printRow + "4"
+                delay = 0.25
+            else:
+                printRow = printRow + "X"
+                delay = 1
+            #moveGrid(x, y, gridIn, checkXShift, checkYShift) # Shows where the image is bieng checked, only works if the below delay line is uncommented
+            #time.sleep(delay)
+        print(printRow)
 
 # Waits until the destruction particles have gone away for a good scan
 def waitForDebris():
