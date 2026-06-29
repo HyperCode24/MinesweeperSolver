2af78becef9618a5e1d2360695c99bd255ab055a
HyperCube24 <hypercube24official@gmail.com>
2026-06-29 11:03:19 -0700

    Added old code to the tree

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+from random import randint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import PIL
+import pyscreeze
+import pyautogui
+import pydirectinput
+import pydirectinput as direct
+import time
 
+'''
+Top left: -1172 808
+Bottom right: -766 1124
+'''
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+'''
+while True:
+    xp, xy = pyautogui.position()
+    print(str(pyautogui.position()) + "\n" + str(pyautogui.pixel(xp,xy)))
+'''
 
+#Get the two important coordianates
+input("Hit enter when you are ready for top left corner")
+x1, y1 = pyautogui.position()
+print("Top left set to: " + str(x1) + " " + str(y1))
+input("Hit enter when you are ready for bottom right corner")
+x2, y2 = pyautogui.position()
+print("Bottom right set to: " + str(x2) + " " + str(y2))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def grid(x,y):
+    #print("Trying to move to x: " + str(x) + " y: " + str(y))
+    xgo = x1 + ((x-1) * ((x2-x1) / 9))
+    ygo = y1 + ((y-1) * ((y2-y1) / 7))
+    xgo = round(xgo)
+    ygo = round(ygo)
+    #print("Moving to: " + str(xgo) + " " + str(ygo))
+    pydirectinput.moveTo(xgo,ygo)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pydirectinput.FAILSAFE = True
+pydirectinput.PAUSE = 0
+
+print("Beginning sequence in 2 seconds")
+time.sleep(2)
